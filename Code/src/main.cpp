54b705617ae52df6cbe9993b2fe07ffd42160c59
BrickStacking <62228212+BrickStacking@users.noreply.github.com>
2020-04-22 16:06:25 +0700

    Second update
    Sync physical buttons with a server in Sinric and Adafruit
    Update in server every 2s.

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -52,13 +52,48 @@ const int relayPin1 = 16; //Change from  pin 14 to test led built-in
 const int relayPin2 = 15;
 const int relayPin3 = 3;
 const int relayPin4 = 13;
-
+/* Variables Use for shift-out74HC595---------------------------------------------------------*/
+bool value1, value2, value3, value4, value5, value6, enable_sensor1, enable_sensor2;
+int value; //Sum value to shift-out
+int latchPin = 14;
+//chân SH_CP(11) của 74HC595
+int clockPin = 12;
+//Chân DS (14)của 74HC595
+int dataPin = 16;
+/* Private function prototypes -----------------------------------------------*/
+void shiftout_74hc595(void);
 void button_processing();
 void setPowerStateOnServer(String deviceId, String value);
 void setTargetTemperatureOnServer(String deviceId, String value, String scale);
 void MQTT_connect();
 // deviceId is the ID assgined to your smart-home-device in sinric.com dashboard. Copy it from dashboard and paste it here
+ICACHE_RAM_ATTR void handleInterrupt1()
+{
+  value1 = !value1;
+  delay(30);
+  shiftout_74hc595();
+}
 
+ICACHE_RAM_ATTR void handleInterrupt2()
+{
+  value2 = !value2;
+  delay(30);
+  shiftout_74hc595();
+}
+
+ICACHE_RAM_ATTR void handleInterrupt3()
+{
+  value3 = !value3;
+  delay(30);
+  shiftout_74hc595();
+}
+
+ICACHE_RAM_ATTR void handleInterrupt4()
+{
+  value4 = !value4;
+  delay(30);
+  shiftout_74hc595();
+}
 void turnOn(String deviceId)
 {
   if (deviceId == "5bb4288477057d2175972428") // Device ID of first device
@@ -66,7 +101,8 @@ void turnOn(String deviceId)
     Serial.println("Turn on relay 1 ");
     Serial.println("Turn on device id: ");
     Serial.println(deviceId);
-    digitalWrite(relayPin1, HIGH);
+    value1 = 1;
+    shiftout_74hc595();
     setPowerStateOnServer("5bb4288477057d2175972428", "ON");
     if (!relay_1.publish("on"))
     {
@@ -82,7 +118,8 @@ void turnOn(String deviceId)
     Serial.println("Turn on relay 2 ");
     Serial.println("Turn on device id: ");
     Serial.println(deviceId);
-    digitalWrite(relayPin2, HIGH);
+    value2 = 1;
+    shiftout_74hc595();
     setPowerStateOnServer("5bb42d6c77057d217597242e", "ON");
     if (!relay_2.publish("on"))
     {
@@ -98,7 +135,8 @@ void turnOn(String deviceId)
     Serial.println("Turn on relay 3 ");
     Serial.println("Turn on device id: ");
     Serial.println(deviceId);
-    digitalWrite(relayPin3, HIGH);
+    value3 = 1;
+    shiftout_74hc595();
     setPowerStateOnServer("5bb42d7e77057d217597242f", "ON");
     if (!relay_3.publish("on"))
     {
@@ -114,7 +152,8 @@ void turnOn(String deviceId)
     Serial.println("Turn on relay 4 ");
     Serial.println("Turn on device id: ");
     Serial.println(deviceId);
-    digitalWrite(relayPin4, HIGH);
+    value4 = 1;
+    shiftout_74hc595();
     setPowerStateOnServer("5bb42d8977057d2175972430", "ON");
     if (!relay_4.publish("on"))
     {
@@ -130,10 +169,11 @@ void turnOn(String deviceId)
     Serial.println("Turn on all relays ");
     Serial.println("Turn on device id: ");
     Serial.println(deviceId);
-    digitalWrite(relayPin1, HIGH);
-    digitalWrite(relayPin2, HIGH);
-    digitalWrite(relayPin3, HIGH);
-    digitalWrite(relayPin4, HIGH);
+    value1 = 1;
+    value2 = 1;
+    value3 = 1;
+    value4 = 1;
+    shiftout_74hc595();
     setPowerStateOnServer("5bb42d6c77057d217597242e", "ON");
     setPowerStateOnServer("5bb42d7e77057d217597242f", "ON");
     setPowerStateOnServer("5bb42d8977057d2175972430", "ON");
@@ -153,7 +193,8 @@ void turnOff(String deviceId)
     Serial.println("Turn off relay 1 ");
     Serial.println("Turn off Device ID: ");
     Serial.println(deviceId);
-    digitalWrite(relayPin1, LOW);
+    value1 = 0;
+    shiftout_74hc595();
     setPowerStateOnServer("5bb4288477057d2175972428", "OFF");
     if (!relay_1.publish("off"))
     {
@@ -169,7 +210,8 @@ void turnOff(String deviceId)
     Serial.println("Turn off relay 2 ");
     Serial.println("Turn off Device ID: ");
     Serial.println(deviceId);
-    digitalWrite(relayPin2, LOW);
+    value2 = 0;
+    shiftout_74hc595();
     setPowerStateOnServer("5bb42d6c77057d217597242e", "OFF");
     if (!relay_2.publish("off"))
     {
@@ -185,7 +227,8 @@ void turnOff(String deviceId)
     Serial.println("Turn off relay 3 ");
     Serial.println("Turn off Device ID: ");
     Serial.println(deviceId);
-    digitalWrite(relayPin3, LOW);
+    value3 = 0;
+    shiftout_74hc595();
     setPowerStateOnServer("5bb42d7e77057d217597242f", "OFF");
     if (!relay_3.publish("off"))
     {
@@ -201,7 +244,8 @@ void turnOff(String deviceId)
     Serial.println("Turn off relay 4 ");
     Serial.println("Turn off Device ID: ");
     Serial.println(deviceId);
-    digitalWrite(relayPin4, LOW);
+    value4 = 0;
+    shiftout_74hc595();
     setPowerStateOnServer("5bb42d8977057d2175972430", "OFF");
     if (!relay_4.publish("off"))
     {
@@ -217,10 +261,11 @@ void turnOff(String deviceId)
     Serial.println("Turn off all relays ");
     Serial.println("Turn off device id: ");
     Serial.println(deviceId);
-    digitalWrite(relayPin1, LOW);
-    digitalWrite(relayPin2, LOW);
-    digitalWrite(relayPin3, LOW);
-    digitalWrite(relayPin4, LOW);
+    value1 = 0;
+    value2 = 0;
+    value3 = 0;
+    value4 = 0;
+    shiftout_74hc595();
     setPowerStateOnServer("5bb42d6c77057d217597242e", "OFF");
     setPowerStateOnServer("5bb42d7e77057d217597242f", "OFF");
     setPowerStateOnServer("5bb42d8977057d2175972430", "OFF");
@@ -303,22 +348,31 @@ void setup()
   Serial.print("Connecting to Wifi: ");
   Serial.println(MySSID);
   // Relay Pin-Out
-  pinMode(relayPin1, OUTPUT);
-  pinMode(relayPin2, OUTPUT);
-  pinMode(relayPin3, OUTPUT);
-  pinMode(relayPin4, OUTPUT);
+  // pinMode(relayPin1, OUTPUT);
+  // pinMode(relayPin2, OUTPUT);
+  // pinMode(relayPin3, OUTPUT);
+  // pinMode(relayPin4, OUTPUT);
 
-  digitalWrite(relayPin1, LOW);
-  digitalWrite(relayPin2, LOW);
-  digitalWrite(relayPin3, LOW);
-  digitalWrite(relayPin4, LOW);
+  // digitalWrite(relayPin1, LOW);
+  // digitalWrite(relayPin2, LOW);
+  // digitalWrite(relayPin3, LOW);
+  // digitalWrite(relayPin4, LOW);
 
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
   //Button Pins
   pinMode(button1, INPUT_PULLUP); //Button1
   pinMode(button2, INPUT_PULLUP); //Button2
   pinMode(button3, INPUT_PULLUP); //Button3
   pinMode(button4, INPUT_PULLUP); //Button4
-
+  // attachInterrupt(digitalPinToInterrupt(button1), handleInterrupt1, FALLING);
+  // attachInterrupt(digitalPinToInterrupt(button2), handleInterrupt2, FALLING);
+  // attachInterrupt(digitalPinToInterrupt(button3), handleInterrupt3, FALLING);
+  // attachInterrupt(digitalPinToInterrupt(button4), handleInterrupt4, FALLING);
+  // attachInterrupt(digitalPinToInterrupt(button2), handleInterrupt2, FALLING);
+  // attachInterrupt(digitalPinToInterrupt(button3), handleInterrupt3, FALLING);
+  // attachInterrupt(digitalPinToInterrupt(button4), handleInterrupt4, FALLING);
   // Waiting for Wifi connect
   while (WiFiMulti.run() != WL_CONNECTED)
   {
@@ -350,8 +404,11 @@ void setup()
 
 void loop()
 {
+  shiftout_74hc595();
+  button_processing();
   webSocket.loop();
   MQTT_connect();
+  Serial.println("None loop");
   if (isConnected)
   {
     // Send heartbeat in order to avoid disconnections during ISP resetting IPs over night. Thanks @MacSass
@@ -360,15 +417,15 @@ void loop()
       heartbeatTimestamp = millis();
       webSocket.sendTXT("H");
     }
+    Serial.println("Loop in adafruit");
   }
-
-  button_processing();
 
   //MQTT Io.adafruit
 
   Adafruit_MQTT_Subscribe *subscription;
-  while ((subscription = mqtt.readSubscription(2000)))
+  while ((subscription = mqtt.readSubscription(1000)))
   {
+    Serial.println("Loop in sinric");
     // Check if its the onoff button feed
     if (subscription == &relay1)
     { //onoff_relay1
@@ -379,14 +436,17 @@ void loop()
       {
         //digitalWrite(Relay1, LOW);
         Serial.println("Turn on relay1 from IO.adafruit");
+        value1 = 1;
+        shiftout_74hc595();
       }
       if (strcmp((char *)relay1.lastread, "off") == 0)
       {
         //digitalWrite(Relay1, HIGH);
         Serial.println("Turn off relay1 from IO.adafruit");
+        value1 = 0;
+        shiftout_74hc595();
       }
     }
-
     // check if its the slider feed
     if (subscription == &relay2)
     { //onoff_relay1
@@ -396,15 +456,18 @@ void loop()
       if (strcmp((char *)relay2.lastread, "on") == 0)
       {
         Serial.println("Turn on relay2 from IO.adafruit");
+        value2 = 1;
+        shiftout_74hc595();
         //digitalWrite(Relay2, LOW);
       }
       if (strcmp((char *)relay2.lastread, "off") == 0)
       {
         Serial.println("Turn off relay2 from IO.adafruit");
+        value2 = 0;
+        shiftout_74hc595();
         //digitalWrite(Relay2, HIGH);
       }
     }
-
     if (subscription == &relay3)
     { //onoff_relay1
       Serial.print(F("On-Off button: "));
@@ -413,15 +476,18 @@ void loop()
       if (strcmp((char *)relay3.lastread, "on") == 0)
       {
         Serial.println("Turn on relay3 from IO.adafruit");
+        value3 = 1;
+        shiftout_74hc595();
         //digitalWrite(Relay2, LOW);
       }
       if (strcmp((char *)relay3.lastread, "off") == 0)
       {
         Serial.println("Turn off relay3 from IO.adafruit");
+        value3 = 0;
+        shiftout_74hc595();
         //digitalWrite(Relay2, HIGH);
       }
     }
-
     if (subscription == &relay4)
     { //onoff_relay1
       Serial.print(F("On-Off button: "));
@@ -430,11 +496,15 @@ void loop()
       if (strcmp((char *)relay4.lastread, "on") == 0)
       {
         Serial.println("Turn on relay4 from IO.adafruit");
+        value4 = 1;
+        shiftout_74hc595();
         //digitalWrite(Relay2, LOW);
       }
       if (strcmp((char *)relay4.lastread, "off") == 0)
       {
         Serial.println("Turn off relay4 from IO.adafruit");
+        value4 = 0;
+        shiftout_74hc595();
         //digitalWrite(Relay2, HIGH);
       }
     }
@@ -479,43 +549,167 @@ void setTargetTemperatureOnServer(String deviceId, String value, String scale)
 void button_processing()
 {
   /*-----------------*/ // Relay and button 1
-  if (digitalRead(button1) == 1)
-    a = 0;
+                        // if (digitalRead(button1) == 1)
+                        //   a = 0;
+                        // if (digitalRead(button1) == 0)
+                        //   a++;
+                        // if (a == 1)
+                        // {
+                        //   //digitalWrite(relayPin1, !digitalRead(relayPin1));
+                        //   value1 = !value1;
+                        //   delay(40);
+                        // }
   if (digitalRead(button1) == 0)
-    a++;
-  if (a == 15)
-    digitalWrite(relayPin1, !digitalRead(relayPin1));
+  {
+    value1 = !value1;
+    shiftout_74hc595();
+    if (1 == value1)
+    {
+      if (!relay_1.publish("on"))
+      {
+        Serial.println(F("Failed to sync from alexa to google home relay 1 ON"));
+      }
+      else
+      {
+        Serial.println(F("Sync from alexa to google home relay 1 ON"));
+      }
+    }
+    if (0 == value1)
+    {
+      if (!relay_1.publish("off"))
+      {
+        Serial.println(F("Failed to sync from alexa to google home relay 1 OFF "));
+      }
+      else
+      {
+        Serial.println(F("Sync from alexa to google home relay 1 OFF"));
+      }
+    }
+  }
   //Sau khi toggle phai update trang thai nut nhan len server
 
   //////////
 
   /*-----------------*/ // Relay and button 2
-  if (digitalRead(button2) == 1)
-    b = 0;
+                        // if (digitalRead(button2) == 1)
+                        //   b = 0;
+                        // if (digitalRead(button2) == 0)
+                        //   b++;
+                        // if (b == 1)
+                        // {
+                        //   // digitalWrite(relayPin2, !digitalRead(relayPin2));
+                        //   value2 = !value2;
+                        //   delay(40);
+                        // }
   if (digitalRead(button2) == 0)
-    b++;
-  if (b == 15)
-    digitalWrite(relayPin2, !digitalRead(relayPin2));
+  {
+    value2 = !value2;
+    shiftout_74hc595();
 
+    if (1 == value2)
+    {
+      if (!relay_2.publish("on"))
+      {
+        Serial.println(F("Failed to sync from alexa to google home relay 2 ON"));
+      }
+      else
+      {
+        Serial.println(F("Sync from alexa to google home relay 2 ON"));
+      }
+    }
+    if (0 == value2)
+    {
+      if (!relay_2.publish("off"))
+      {
+        Serial.println(F("Failed to sync from alexa to google home relay 2 OFF "));
+      }
+      else
+      {
+        Serial.println(F("Sync from alexa to google home relay 2 OFF"));
+      }
+    }
+  }
   //////////
 
   /*-----------------*/ // Relay and button 3
-  if (digitalRead(button3) == 1)
-    c = 0;
+  // if (digitalRead(button3) == 1)
+  //   c = 0;
+  // if (digitalRead(button3) == 0)
+  //   c++;
+  // if (c == 1)
+  // {
+  //   // digitalWrite(relayPin3, !digitalRead(relayPin3));
+  //   value3 = !value3;
+  //   delay(40);
+  // }
   if (digitalRead(button3) == 0)
-    c++;
-  if (c == 15)
-    digitalWrite(relayPin3, !digitalRead(relayPin3));
+  {
+    value3 = !value3;
+    shiftout_74hc595();
+if (1 == value3)
+    {
+      if (!relay_3.publish("on"))
+      {
+        Serial.println(F("Failed to sync from alexa to google home relay 3 ON"));
+      }
+      else
+      {
+        Serial.println(F("Sync from alexa to google home relay 3 ON"));
+      }
+    }
+    if (0 == value3)
+    {
+      if (!relay_3.publish("off"))
+      {
+        Serial.println(F("Failed to sync from alexa to google home relay 3 OFF "));
+      }
+      else
+      {
+        Serial.println(F("Sync from alexa to google home relay 3 OFF"));
+      }
+    }
+  }
 
   //////////
 
   /*-----------------*/ // Relay and button 4
-  if (digitalRead(button4) == 1)
-    d = 0;
   if (digitalRead(button4) == 0)
-    d++;
-  if (d == 15)
-    digitalWrite(relayPin4, !digitalRead(relayPin4));
+  {
+    value4 = !value4;
+    shiftout_74hc595();
+if (1 == value4)
+    {
+      if (!relay_4.publish("on"))
+      {
+        Serial.println(F("Failed to sync from alexa to google home relay 4 ON"));
+      }
+      else
+      {
+        Serial.println(F("Sync from alexa to google home relay 4 ON"));
+      }
+    }
+    if (0 == value4)
+    {
+      if (!relay_4.publish("off"))
+      {
+        Serial.println(F("Failed to sync from alexa to google home relay 4 OFF "));
+      }
+      else
+      {
+        Serial.println(F("Sync from alexa to google home relay 4 OFF"));
+      }
+    }
+  }
+  // if (digitalRead(button4) == 1)
+  //   d = 0;
+  // if (digitalRead(button4) == 0)
+  //   d++;
+  // if (d == 1)
+  // {
+  //   // digitalWrite(relayPin4, !digitalRead(relayPin4));
+  //   value4 = !value4;
+  //   delay(40);
+  // }
 
   //////////
 }
@@ -538,7 +732,7 @@ void MQTT_connect()
     Serial.println(mqtt.connectErrorString(ret));
     Serial.println("Retrying MQTT connection in 5 seconds...");
     mqtt.disconnect();
-    delay(5000); // wait 5 seconds
+    delay(1000); // wait 5 seconds
     retries--;
     if (retries == 0)
     {
@@ -548,4 +742,13 @@ void MQTT_connect()
     }
   }
   Serial.println("MQTT Connected!");
+}
+
+void shiftout_74hc595(void)
+{
+  value = value1 * 2 + value2 * 4 + value3 * 8 + value4 * 16;
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, value);
+  //shiftOut(dataPin, clockPin, MSBFIRST, so1[mode1]);
+  digitalWrite(latchPin, HIGH);
 }
